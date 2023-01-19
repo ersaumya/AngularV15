4b4efe837d3c1f93415b99f3b86f4250129a0540
SOUMYA\ersau <ersaumyarout@gmail.com>
2023-01-19 18:41:00 +0530

    convert to standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
+  imports:[RouterModule,CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app-routing';
+
 import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
 
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent,{
+  providers:[importProvidersFrom (RouterModule.forRoot(routes))]
+})
   .catch(err => console.error(err));
